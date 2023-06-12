f2392db774db86279c18e023fce7f6a94c964f48
Leticia Santos Moura <letistos@gmail.com>
2023-06-12 18:02:38 -0300

    rotas entre já tem cadastro e cadastre-se

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from 'axios';
 import styled from "styled-components";
 import Start from './components/Start'
 import Signup from './pages/Sign-up'
 import Subscriptions from "./pages/Subscriptions";
 
 function App() {
-
+  axios.defaults.headers.common['Authorization'] = '5MH3GBE7JWFZU8b8ft1wY2FM'
   return (
     
     <Body>
--- src/pages/Sign-up.jsx
+++ src/pages/Sign-up.jsx
@@ -1,12 +1,51 @@
-import teste from '../assets/teste.jpg'
-
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function Signup(){
     return(
-    <>
-    <img src={teste}/>
-    </>
+    <Container>
+    <Form>
+        <Formulario type='name' required placeholder="Nome"></Formulario>
+        <Formulario type='number' required placeholder="CPF"></Formulario>
+        <Formulario type='email' required placeholder="E-mail"></Formulario>
+        <Formulario type='password' required placeholder="Senha"></Formulario>
+    </Form>
+        <Botao>CADASTRAR</Botao>
+        <Link to='/'>Já possuí uma conta? Entre</Link>
+    </Container>
     );
 }
 export default Signup;
 
+const Container = styled.div`
+    height:667px;
+    width:375px;
+    background-color:#0E0E13;
+    display:flex;
+    align-items:center;
+    flex-direction:column;
+    justify-content:center;
+`
+const Formulario = styled.input`
+    width:299px;
+    height:52px;
+    border-radius:8px;
+    margin:16px;
+`
+const Botao = styled.button`
+    width:308px;
+    height:60px;
+    border-radius:8px;
+    background-color:#FF4791;
+    margin:16px;
+`
+const Form = styled.form`
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+`
+const Image = styled.img`
+    width:299px;
+    height:49px;
+    margin-bottom:100px;
+`
