63da52a8704a518295ce04e49b654cab0caea234
Leticia Santos Moura <letistos@gmail.com>
2023-06-13 15:05:08 -0300

    página subscriptions, layout

--- src/components/Planos.jsx
+++ src/components/Planos.jsx
@@ -9,20 +9,59 @@ function Planos(){
     const logos=[branco,amarelo,verde];    
 
     return (
-        <ul>
+        <Container>
+            <Titulo>Escolha seu Plano</Titulo>
             {dados.map((opcao, i) => (
 
                 <li key={opcao.plano}>
-                    <div>
+                    <Box>
                         <img src={logos[i]} alt="logo planos driven plus" />
                         <p>{opcao.preco}</p>
-                    </div>
+                    </Box>
                 </li>
             ))
             };
 
-        </ul>
+        </Container>
     );
 }
 export default Planos
 
+const Box = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    width:290px;
+    height:180px;
+    border:3px solid #7E7E7E;
+    border-radius:12px;
+    margin-top:10px;
+
+
+    p{
+        font-family:'Roboto';
+        font-weight:700;
+        font-size:24px;
+        line-height:28.13px;
+        color:#FFFFFF;
+    }
+    img{
+        margin-right:15px;
+    }
+`
+const Container = styled.ul`
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    align-items:center;
+    margin-top:10px;
+    
+`
+const Titulo = styled.p`
+    font-family:'Roboto';
+    font-weight:700;
+    font-size:32px;
+    line-height:37.5px;
+    color:#FFFFFF;
+
+`
